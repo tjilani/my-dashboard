ea8ba3c7b44e4da4aa54450db256cfe3af39870e
Tariq <tariq.jilani@protonmail.com>
2026-01-19 18:17:58 +0100

    basic styling for Book component

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
-const Book = ({work}: {work: {id: string, title: string, author: string}}) => {
+const Book = ({
+  work,
+}: {
+  work: { id: string; title: string; author: string };
+}) => {
   return (
-    <div key={work.id}>
-    <h2>{work.title}</h2>
-</div>
-  )
-}
+    <StyledBook key={work.id}>
+      <h2>{work.title}</h2>
+      <h4>{work.author}</h4>
+      <button>Details</button>
+    </StyledBook>
+  );
+};
 
-export default Book
+export default Book;
+
+const StyledBook = styled.article`
+  background-color: #1f7a8c;
+  padding: 1rem 1%.5rem;
+  border-radius: 5px;
+  margin: 1.5rem;
+  width: 70vw;
+  height: 100px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
