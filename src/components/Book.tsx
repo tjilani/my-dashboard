9d9bc38c6c3f183c712815b4570087fbbc29e414
Tariq <tariq.jilani@protonmail.com>
2026-01-20 17:28:14 +0100

    books.json expanded, display cover-images in Book component

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,13 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 
+// Import all cover images using Vite's glob import
+const coverImages = import.meta.glob("../assets/covers/*.{jpg,png}", {
+  eager: true,
+  import: "default",
+}) as Record<string, string>;
+
+const getCoverImage = (path: string): string => {
+  console.log("path:", path);
+  console.log("coverImage:", coverImages[path]);
+  return coverImages[path];
+};
+
 const Book = ({
   work,
 }: {
-  work: { id: string; title: string; author: string };
+  work: { id: string; title: string; author: string; cover_small: string };
 }) => {
+  const coverImage = getCoverImage(work.cover_small);
   return (
     <StyledBook key={work.id}>
+      <div className="image-box">
+        <img src={coverImage} alt={work.title} />
+      </div>
       <h2>{work.title}</h2>
       <h4>{work.author}</h4>
       <button>Details</button>
@@ -28,4 +44,16 @@ const StyledBook = styled.article`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
+  .image-box {
+    width: 100px;
+    height: 100px;
+    background-color: #fff;
+    border-radius: 5px;
+    overflow: hidden;
+  }
+  .image-box img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 `;
--- src/pages/BooksOverview.tsx
+++ src/pages/BooksOverview.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import books from '../data/books.json'
-import Book from '../components/Book'
+import React from "react";
+import books from "../data/books.json";
+import Book from "../components/Book";
 
 const BooksOverview = () => {
   return (
     <div>
-        {books.map((book) => (
-            <Book key={book.work.id} work={book.work}  />
-        ))}
+      {books.map((book) => (
+        <Book key={book.work.id} work={book.work} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default BooksOverview
+export default BooksOverview;
